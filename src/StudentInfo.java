34e8f6e48bb0384c54a8171a50f9e37629561f52
박지훈 <@DESKTOP-U7E72EH>
2022-07-26 10:47:13 +0900

    ddd

--- src/StudentInfo.java
+++ src/StudentInfo.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class StudentInfo {
 	public static void main(String[] args) {	// 프로그램의 시작과 끝
 		Scanner sc = new Scanner(System.in);
-		System.out.print("오늘 날짜는? : ");
-		String today = sc.nextLine();
 		
 		System.out.print("학생 이름 : ");
 		String name = sc.nextLine();
@@ -15,7 +13,6 @@ public class StudentInfo {
 		System.out.print("주소 : ");
 		String address = sc.nextLine();
 		
-		System.out.println("날짜 : " + today);
 		System.out.println("이름:  " + name);
 		System.out.println("학번 : " + code);
 		System.out.println("주소 : " + address);
