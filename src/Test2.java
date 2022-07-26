7df36ddaf1f95b9d5b4538b2de0cd12f8414c298
박지훈 <@DESKTOP-U7E72EH>
2022-07-26 17:50:59 +0900

    test 2

--- src/Test2.java
+++ src/Test2.java
@@ -4,12 +4,10 @@ public class Test2 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("문자 : ");
-		String ch = sc.nextLine();
+		String ch = sc.nextLine();		// "A"
+		char ch1 = ch.charAt(0);
 		
-		System.out.printf("%c", ch);
-		
-		
-		
+		System.out.printf("문자 %c의 코드값은 %d입니다. " ,ch1 ,(int)(ch1));
 
 	}
 
